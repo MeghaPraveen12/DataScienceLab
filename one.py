82c340e465994bef569f3668e0fbb8be3d709a00
MeghaPraveen12 <125427652+MeghaPraveen12@users.noreply.github.com>
2023-10-10 15:00:49 +0530

    Add files via upload

---
+++ one.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+print("Megha Praveen")
+print("SJC22MCA-2039")
+print("MCA 2022-2024")
+
+array_3d = np.array([
+    [
+        [1.0, 2.0, 3.0, 4.0],
+        [5.0, 6.0, 7.0, 8.0],
+        [9.0, 10.0, 11.0, 12.0]
+    ],
+    [
+        [13.0, 14.0, 15.0, 16.0],
+        [17.0, 18.0, 19.0, 20.0],
+        [21.0, 22.0, 23.0, 24.0]
+    ]
+], dtype=float)
+print("3D Array:")
+print(array_3d)
